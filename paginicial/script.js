d85ebe99dc8bc98892af6d0631a62fc639960b8a
N1kkoin <nikkonh@hotmail.com>
2023-11-02 18:49:19 -0300

    att

--- paginicial/script.js
+++ paginicial/script.js
@@ -10,7 +10,7 @@ firebase.initializeApp(config);
 
 var dCounters = document.querySelectorAll('.CountLike');
 
-[].forEach.call(dCounters, function(dCounter) {
+[].forEach.call(dCounters, function (dCounter) {
     var el = dCounter.querySelector('button');
     var cId = dCounter.id; // Isso nos dará "LikeCount1", "LikeCount2", etc.
     var dDatabase = firebase.database().ref('Like Number Counter/' + cId); // Salve cada contagem de likes em um nó separado
@@ -27,34 +27,74 @@ var dCounters = document.querySelectorAll('.CountLike');
     }
 
     // Verifique se o usuário já deu like anteriormente
-    usersLikedRef.child(userID).once('value', function(snap) {
+    usersLikedRef.child(userID).once('value', function (snap) {
         if (snap.exists()) {
             el.classList.add('button1-liked');
         }
     });
 
     // get firebase data
-    likesCountRef.on('value', function(snap) {
+    likesCountRef.on('value', function (snap) {
         var data = snap.val() || 0;
         dCounter.querySelector('span').innerHTML = data;
     });
 
     // set firebase data
-    el.addEventListener('click', function() {
-        usersLikedRef.child(userID).once('value', function(snap) {
+    el.addEventListener('click', function () {
+        usersLikedRef.child(userID).once('value', function (snap) {
             if (snap.exists()) {
                 usersLikedRef.child(userID).remove();
-                likesCountRef.transaction(function(currentLikes) {
+                likesCountRef.transaction(function (currentLikes) {
                     return (currentLikes || 0) - 1;
                 });
                 el.classList.remove('button1-liked');
             } else {
                 usersLikedRef.child(userID).set(true);
-                likesCountRef.transaction(function(currentLikes) {
+                likesCountRef.transaction(function (currentLikes) {
                     return (currentLikes || 0) + 1;
                 });
                 el.classList.add('button1-liked');
             }
         });
+    });
+});
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Seleciona todos os elementos .grid-item
+    const gridItems = document.querySelectorAll('.grid-item');
+
+    // Cria um array para armazenar informações sobre os elementos
+    const itemData = [];
+
+    // Itera sobre os elementos .grid-item
+    gridItems.forEach((gridItem, index) => {
+        // Obtém a quantidade de .counterStat do elemento, verificando se existe
+        const counterStat = gridItem.querySelector('.counterStat');
+        if (counterStat) {
+            const likesCount = parseInt(counterStat.textContent || 0);
+
+            // Armazena os dados do elemento no array
+            itemData.push({ element: gridItem, likesCount, index });
+        }
+    });
+
+    // Classifica os elementos com base na quantidade de likesCount em ordem decrescente
+    itemData.sort((a, b) => b.likesCount - a.likesCount);
+    
+    // Crie um novo array para armazenar os 5 principais elementos
+    const top5Items = itemData.slice(0, 5);
+    
+    // Remove todos os elementos .grid-item da seção "Mais Relevantes"
+    const maisRelevantesContainer = document.querySelector('.grid-container');
+    maisRelevantesContainer.innerHTML = '';
+
+    // Adicione os elementos classificados de volta à seção "Mais Relevantes"
+    top5Items.forEach((data) => {
+        maisRelevantesContainer.appendChild(data.element);
     });
 });
