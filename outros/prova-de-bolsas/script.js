6c474c6620f1d16bb303cc7b444cb44430bb6386
NICOLE HEGUY <nikkonh@hotmail.com>
2023-12-04 14:07:27 -0300

    att

--- outros/prova-de-bolsas/script.js
+++ outros/prova-de-bolsas/script.js
@@ -52,3 +52,44 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('scroll', checkVisibility);
   });
   
+
+
+
+  
+  $(document).ready(function () {
+    $(".arrow").click(function () {
+        let direction = $(this).data('direction');
+
+        // Pegando o slide atual visível
+        let currentSlide = $(this).closest('.slideshow-item');
+
+        // Pegando todos os slides
+        let slides = $('.slideshow-item');
+
+        // Encontrando o índice do slide atual
+        let currentIndex = slides.index(currentSlide);
+
+        // Calculando o próximo índice
+        let nextIndex;
+        if (direction === 'next') {
+            nextIndex = (currentIndex + 1) % slides.length;
+        } else {
+            nextIndex = (currentIndex - 1 + slides.length) % slides.length;
+        }
+
+        // Removendo a classe 'visible' do slide atual
+        currentSlide.removeClass('visible');
+
+        // Adicionando a classe 'visible' ao próximo slide
+        slides.eq(nextIndex).addClass('visible');
+    });
+});
+
+
+
+
+
+
+$(document).ready(function () {
+  $("#meuFooter").load("/codigos-gerais/footer/footer.html");
+});
