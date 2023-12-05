044e4eda690374473a65eb06a7d0361606240adf
NICOLE HEGUY <nikkonh@hotmail.com>
2023-12-04 21:20:43 -0300

    att

--- outros/prova-de-bolsas/script.js
+++ outros/prova-de-bolsas/script.js
@@ -1,16 +1,16 @@
-// script.js
+// LOGO MUDAR TAMANHO PC/MOBILE --------------------------------------------------------------------------------------------------------------------------------------------
 function ajustarLogo() {
-    var largura = window.innerWidth;
-    var logoQuadrado = document.getElementById('logoQuadrado');
-    var logoRetangular = document.getElementById('logoRetangular');
+  var largura = window.innerWidth;
+  var logoQuadrado = document.getElementById('logoQuadrado');
+  var logoRetangular = document.getElementById('logoRetangular');
 
-    if (largura < 960) { // Ponto de corte
-        logoQuadrado.style.display = 'none';
-        logoRetangular.style.display = 'block';
-    } else {
-        logoQuadrado.style.display = 'block';
-        logoRetangular.style.display = 'none';
-    }
+  if (largura < 960) { // Ponto de corte
+    logoQuadrado.style.display = 'none';
+    logoRetangular.style.display = 'block';
+  } else {
+    logoQuadrado.style.display = 'block';
+    logoRetangular.style.display = 'none';
+  }
 }
 
 // Executa ao carregar e ao redimensionar a página
@@ -18,89 +18,111 @@ window.onload = ajustarLogo;
 window.onresize = ajustarLogo;
 
 
+// GRÁFICO BOLSAS QUANTIDADE -------------------------------------------------------------------------------------------------------------------
 
-document.addEventListener("DOMContentLoaded", function() {
-    const bars = document.querySelectorAll('.bar-graph-horizontal .bar');
-    const texts = document.querySelectorAll('.bar-graph-horizontal .year');
-  
-    function isElementInViewport(el) {
-      const rect = el.getBoundingClientRect();
-      return (
-        rect.top >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-      );
+document.addEventListener("DOMContentLoaded", function () {
+  const bars = document.querySelectorAll('.bar-graph-horizontal .bar');
+  const texts = document.querySelectorAll('.bar-graph-horizontal .year');
+
+  function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+  }
+
+  function checkVisibility() {
+    texts.forEach((text, index) => {
+      if (isElementInViewport(text)) {
+        text.classList.add('animate-text');
+      }
+    });
+
+    bars.forEach((bar, index) => {
+      if (isElementInViewport(bar)) {
+        bar.classList.add(`animate-bar-${index + 1}`);
+      }
+    });
+  }
+
+  // Verifica a visibilidade dos elementos quando a página é carregada
+  checkVisibility();
+
+  // Verifica a visibilidade dos elementos quando o usuário faz scroll
+  window.addEventListener('scroll', checkVisibility);
+});
+
+
+
+//SLIDESHOW ---------------------------------------------------------------------------------------------------------------
+
+$(document).ready(function () {
+  $(".arrow").click(function () {
+    let direction = $(this).data('direction');
+
+    // Pegando o slide atual visível
+    let currentSlide = $(this).closest('.slideshow-item');
+
+    // Pegando todos os slides
+    let slides = $('.slideshow-item');
+
+    // Encontrando o índice do slide atual
+    let currentIndex = slides.index(currentSlide);
+
+    // Calculando o próximo índice
+    let nextIndex;
+    if (direction === 'next') {
+      nextIndex = (currentIndex + 1) % slides.length;
+    } else {
+      nextIndex = (currentIndex - 1 + slides.length) % slides.length;
     }
-  
-    function checkVisibility() {
-      texts.forEach((text, index) => {
-        if (isElementInViewport(text)) {
-          text.classList.add('animate-text');
-        }
-      });
-  
-      bars.forEach((bar, index) => {
-        if (isElementInViewport(bar)) {
-          bar.classList.add(`animate-bar-${index + 1}`);
-        }
-      });
-    }
-  
-    // Verifica a visibilidade dos elementos quando a página é carregada
-    checkVisibility();
-  
-    // Verifica a visibilidade dos elementos quando o usuário faz scroll
-    window.addEventListener('scroll', checkVisibility);
+
+    // Removendo a classe 'visible' do slide atual
+    currentSlide.removeClass('visible');
+
+    // Adicionando a classe 'visible' ao próximo slide
+    slides.eq(nextIndex).addClass('visible');
   });
-  
-
-
-
-  
-  $(document).ready(function () {
-    $(".arrow").click(function () {
-        let direction = $(this).data('direction');
-
-        // Pegando o slide atual visível
-        let currentSlide = $(this).closest('.slideshow-item');
-
-        // Pegando todos os slides
-        let slides = $('.slideshow-item');
-
-        // Encontrando o índice do slide atual
-        let currentIndex = slides.index(currentSlide);
-
-        // Calculando o próximo índice
-        let nextIndex;
-        if (direction === 'next') {
-            nextIndex = (currentIndex + 1) % slides.length;
-        } else {
-            nextIndex = (currentIndex - 1 + slides.length) % slides.length;
-        }
-
-        // Removendo a classe 'visible' do slide atual
-        currentSlide.removeClass('visible');
-
-        // Adicionando a classe 'visible' ao próximo slide
-        slides.eq(nextIndex).addClass('visible');
-    });
 });
 
 
+//MININAVBAR ------------------------------------------------------------------------------------------
 
-document.addEventListener("DOMContentLoaded", function() {
-    var bg = document.getElementById("buttonBackground");
+document.addEventListener("DOMContentLoaded", function () {
+  var bg = document.getElementById("buttonBackground");
 
-    window.addEventListener("scroll", function() {
-        if (window.scrollY > window.innerHeight) {
-            bg.style.display = "flex"; 
-        } else {
-            bg.style.display = "none";
-        }
-    });
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > window.innerHeight) {
+      bg.style.display = "flex";
+    } else {
+      bg.style.display = "none";
+    }
+  });
 });
 
 
-
+//FOOTER -----------------------------------------------------------------------------------------------------------------------------
 $(document).ready(function () {
   $("#meuFooter").load("/codigos-gerais/footer/footer.html");
 });
+
+
+
+// Data alvo (23 de outubro de 2023)
+const targetDate = new Date('2024-01-21T00:00:00');
+
+function updateCountdown() {
+  const currentDate = new Date();
+  const timeLeft = targetDate - currentDate;
+  const daysLeft = Math.ceil(timeLeft / (1000 * 60 * 60 * 24)); // Usamos Math.ceil para arredondar para cima
+
+  if (daysLeft < 0) {
+    document.getElementById("countdown").textContent = "A prova já acabou, acompanhe nossas redes sociais para que no próximo semestre você não perca a Prova de Bolsas ;)";
+  } else {
+    document.getElementById("countdown").textContent = `Nos vemos em ${daysLeft} dias!`;
+  }
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000 * 60 * 60 * 24); // Atualizar a cada dia
