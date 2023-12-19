dca000323dba9a94f1f0b2b2a62ec6a861ee0362
NICOLE HEGUY <nikkonh@hotmail.com>
2023-12-19 15:31:13 -0300

    add lgpd

--- outros/lgpd/script.js
+++ outros/lgpd/script.js
@@ -1,10 +1,23 @@
-// script.js
 function openModal(modalId) {
     document.getElementById(modalId).style.display = "block";
+    document.body.classList.add("body-no-scroll");
 }
 
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+    document.body.classList.remove("body-no-scroll");
+}
+
+// Fechando o modal quando clicado fora dele
 window.onclick = function(event) {
     if (event.target.classList.contains('modal')) {
-        event.target.style.display = "none";
+        closeModal(event.target.id);
     }
 }
+
+// Fechando o modal quando o botão 'X' é clicado
+document.querySelectorAll('.close-button').forEach(button => {
+    button.onclick = function() {
+        closeModal(this.closest('.modal').id);
+    };
+});
