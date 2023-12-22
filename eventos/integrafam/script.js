7d0a8933f2cb5033254683e2a0ec93e92ada5e5b
NICOLE HEGUY <nikkonh@hotmail.com>
2023-12-22 11:13:49 -0300

    att navbar

--- eventos/integrafam/script.js
+++ eventos/integrafam/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
     $("#meuFooter").load("/codigos-gerais/footer/footer.html");
 });
+
+// Carregando o navbar e configurando o dropdown
+$(document).ready(function () {
+    $("#Navbar").load("/codigos-gerais/navbar/navbar.html", function () {
+        // Chama a função setupDropdown após o conteúdo ser carregado
+        setupDropdown();
+    });
+});
