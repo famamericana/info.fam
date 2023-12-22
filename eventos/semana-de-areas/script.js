e69b6006b738aaa93c0f9ed34be0140fa29bae0b
NICOLE HEGUY <nikkonh@hotmail.com>
2023-12-22 14:04:04 -0300

    att

---
+++ eventos/semana-de-areas/script.js
@@ -0,0 +1,61 @@
+function toggleAccordion(index) {
+    const tabs = document.querySelectorAll('.accordion .tab .content');
+    const currentTab = tabs[index];
+
+    if (currentTab.style.display === 'block') {
+        currentTab.style.display = 'none';
+    } else {
+        for (let tab of tabs) {
+            tab.style.display = 'none';
+        }
+        currentTab.style.display = 'block';
+    }
+}
+
+$(document).ready(function () {
+    $("#meuFooter").load("/codigos-gerais/footer/footer.html");
+});
+
+$(document).ready(function () {
+    $(".arrow").click(function () {
+        let direction = $(this).data('direction');
+
+        // Pegando o slide atual visível
+        let currentSlide = $(this).closest('.slideshow-item');
+
+        // Pegando todos os slides
+        let slides = $('.slideshow-item');
+
+        // Encontrando o índice do slide atual
+        let currentIndex = slides.index(currentSlide);
+
+        // Calculando o próximo índice
+        let nextIndex;
+        if (direction === 'next') {
+            nextIndex = (currentIndex + 1) % slides.length;
+        } else {
+            nextIndex = (currentIndex - 1 + slides.length) % slides.length;
+        }
+
+        // Removendo a classe 'visible' do slide atual
+        currentSlide.removeClass('visible');
+
+        // Adicionando a classe 'visible' ao próximo slide
+        slides.eq(nextIndex).addClass('visible');
+    });
+});
+
+
+
+// Carregando o navbar e configurando o dropdown
+$(document).ready(function () {
+$("#Navbar").load("/codigos-gerais/navbar/navbar.html", function () {
+// Chama a função setupDropdown após o conteúdo ser carregado
+setupDropdown();
+});
+});
+
+//https forçar -------------------------------------------------------------------------------------------------------------------------------
+if (!location.href.startsWith("http://127.0") && location.protocol !== 'https:') {
+location.replace(`https:${location.href.substring(location.protocol.length)}`);
+}
