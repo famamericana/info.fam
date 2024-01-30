3659473a8e05550b600ff510aa86c8f92ed638a8
NICOLE HEGUY <nikkonh@hotmail.com>
2024-01-30 13:57:49 -0300

    att

--- cursos/script.js
+++ cursos/script.js
@@ -1,38 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
     const cursos = [
-        { nome: "Administração",  semestres: 8, presencialead: ["Presencial | EaD -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "humanas"], descricao: "Descrição do curso de Administração.", link: "link-administracao.html" },
-        { nome: "Biomedicina",  semestres: 8, presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Biomedicina.", link: "link-biomedicina.html" },
-        { nome: "Ciência da Computação",   semestres: 8, presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas"], descricao: "Descrição do curso de Ciência da Computação.", link: "link-ciencia-computacao.html" },
-        { nome: "Ciências Contábeis",   semestres: 8, presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas"], descricao: "Descrição do curso de Ciências Contábeis.", link: "link-ciencias-contabeis.html" },
-        { nome: "Comunicação Social - Publicidade e Propaganda",  semestres: 8, presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "humanas"], descricao: "Descrição do curso de Comunicação Social.", link: "link-comunicacao-social.html" },
-        { nome: "Direito",  semestres: 10, presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "leitura"], descricao: "Descrição do curso de Direito.", link: "link-direito.html" },
-        { nome: "Design Gráfico",  semestres: 4, presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "humanas"], descricao: "Descrição do curso de Design.", link: "link-direito.html" },
-        { nome: "Educação Física",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Educação Física.", link: "link-educacao-fisica.html" },
-        { nome: "Educação Física",  presencialead: ["Presencial -"], tipodegraduacao: ["Licenciatura"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Educação Física.", link: "link-educacao-fisica.html" },
-        { nome: "Enfermagem",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Enfermagem.", link: "link-enfermagem.html" },
-        { nome: "Engenharia Civil",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas", "contas"], descricao: "Descrição do curso de Engenharia Civil.", link: "link-engenharia-civil.html" },
-        { nome: "Engenharia de Produção",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas"], descricao: "Descrição do curso de Engenharia de Produção.", link: "link-engenharia-producao.html" },
-        { nome: "Engenharia Elétrica",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas"], descricao: "Descrição do curso de Engenharia Elétrica.", link: "link-engenharia-eletrica.html" },
-        { nome: "Engenharia Mecânica",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas"], descricao: "Descrição do curso de Engenharia Mecânica.", link: "link-engenharia-mecanica.html" },
-        { nome: "Engenharia Química",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas"], descricao: "Descrição do curso de Engenharia Química.", link: "link-engenharia-quimica.html" },
-        { nome: "Farmácia",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Farmácia.", link: "link-farmacia.html" },
-        { nome: "Fisioterapia",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Fisioterapia.", link: "link-fisioterapia.html" },
-        { nome: "Letras",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "humanas", "leitura"], descricao: "Descrição do curso de Letras.", link: "link-letras.html" },
-        { nome: "Medicina Veterinária (matutino)",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Medicina Veterinária (matutino).", link: "link-medicina-veterinaria.html" },
-        { nome: "Medicina Veterinária (noturno)",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Medicina Veterinária (noturno).", link: "link-medicina-veterinaria.html" },
-        { nome: "Nutrição",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Nutrição.", link: "link-nutricao.html" },
-        { nome: "Pedagogia",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "educação"], descricao: "Descrição do curso de Pedagogia.", link: "link-pedagogia.html" },
-        { nome: "Pedagogia EAD",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["ead", "educação"], descricao: "Descrição do curso de Pedagogia EAD.", link: "link-pedagogia-ead.html" },
-        { nome: "Psicologia",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Psicologia.", link: "link-psicologia.html" },
-        { nome: "Sistemas de Informação",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas"], descricao: "Descrição do curso de Sistemas de Informação.", link: "link-sistemas-informacao.html" },
-        { nome: "Recursos Humanos",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "educação"], descricao: "Descrição do curso de Recursos Humanos.", link: "link-recursos-humanos.html" },
-        { nome: "Recursos Humanos EAD",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["ead", "educação"], descricao: "Descrição do curso de Recursos Humanos EAD.", link: "link-recursos-humanos-ead.html" },
-        { nome: "Pós Marketing",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["pos-graduação", "educação"], descricao: "Descrição do curso de Pós Marketing.", link: "link-pos-marketing.html" },
-        { nome: "Pós Equina",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["pos-graduação", "saúde"], descricao: "Descrição do curso de Pós Equina.", link: "link-pos-equina.html" },
-        { nome: "Pós Neuro",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["pos-graduação", "saúde"], descricao: "Descrição do curso de Pós Neuro.", link: "link-pos-neuro.html" },
-        { nome: "Pós PsicoEscolar",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["pos-graduação", "educação"], descricao: "Descrição do curso de Pós PsicoEscolar.", link: "link-pos-psicoescolar.html" },
-        { nome: "Pós MBAFinanças",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["pos-graduação", "exatas"], descricao: "Descrição do curso de Pós MBAFinanças.", link: "link-pos-mbafinancas.html" },
-        { nome: "Pós EstraFiscal",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["pos-graduação", "exatas"], descricao: "Descrição do curso de Pós EstraFiscal.", link: "link-pos-estrafiscal.html" }
+        { codigogoogle: ["Administração", "Administração EAD"] , nome: "Administração",  semestres: 8, presencialead: ["Presencial | EaD -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "humanas"], descricao: "Descrição do curso de Administração.", link: "link-administracao.html" },
+        { codigogoogle: ["Biomedicina"], nome: "Biomedicina",  semestres: 8, presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Biomedicina.", link: "link-biomedicina.html" },
+        { codigogoogle: ["Ciência da Computação"], nome: "Ciência da Computação",   semestres: 8, presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas"], descricao: "Descrição do curso de Ciência da Computação.", link: "link-ciencia-computacao.html" },
+        { codigogoogle: ["Ciências Contábeis"], nome: "Ciências Contábeis",   semestres: 8, presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas"], descricao: "Descrição do curso de Ciências Contábeis.", link: "link-ciencias-contabeis.html" },
+        { codigogoogle: ["Comunicação Social"], nome: "Comunicação Social - Publicidade e Propaganda",  semestres: 8, presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "humanas"], descricao: "Descrição do curso de Comunicação Social.", link: "link-comunicacao-social.html" },
+        { codigogoogle: ["Direito"], nome: "Direito",  semestres: 10, presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "leitura"], descricao: "Descrição do curso de Direito.", link: "link-direito.html" },
+        { codigogoogle: ["Design Gráfico"], nome: "Design Gráfico",  semestres: 4, presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "humanas"], descricao: "Descrição do curso de Design.", link: "link-direito.html" },
+        { codigogoogle: ["Educação Física"], nome: "Educação Física",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Educação Física.", link: "link-educacao-fisica.html" },
+        { codigogoogle: ["Educação Física"], nome: "Educação Física",  presencialead: ["Presencial -"], tipodegraduacao: ["Licenciatura"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Educação Física.", link: "link-educacao-fisica.html" },
+        { codigogoogle: [""], nome: "Enfermagem",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Enfermagem.", link: "link-enfermagem.html" },
+        { codigogoogle: [""], nome: "Engenharia Civil",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas", "contas"], descricao: "Descrição do curso de Engenharia Civil.", link: "link-engenharia-civil.html" },
+        { codigogoogle: [""], nome: "Engenharia de Produção",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas"], descricao: "Descrição do curso de Engenharia de Produção.", link: "link-engenharia-producao.html" },
+        { codigogoogle: [""], nome: "Engenharia Elétrica",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas"], descricao: "Descrição do curso de Engenharia Elétrica.", link: "link-engenharia-eletrica.html" },
+        { codigogoogle: [""], nome: "Engenharia Mecânica",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas"], descricao: "Descrição do curso de Engenharia Mecânica.", link: "link-engenharia-mecanica.html" },
+        { codigogoogle: [""], nome: "Engenharia Química",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas"], descricao: "Descrição do curso de Engenharia Química.", link: "link-engenharia-quimica.html" },
+        { codigogoogle: [""], nome: "Farmácia",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Farmácia.", link: "link-farmacia.html" },
+        { codigogoogle: [""], nome: "Fisioterapia",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Fisioterapia.", link: "link-fisioterapia.html" },
+        { codigogoogle: [""], nome: "Letras",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "humanas", "leitura"], descricao: "Descrição do curso de Letras.", link: "link-letras.html" },
+        { codigogoogle: [""], nome: "Medicina Veterinária (matutino)",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Medicina Veterinária (matutino).", link: "link-medicina-veterinaria.html" },
+        { codigogoogle: [""], nome: "Medicina Veterinária (noturno)",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Medicina Veterinária (noturno).", link: "link-medicina-veterinaria.html" },
+        { codigogoogle: [""], nome: "Nutrição",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Nutrição.", link: "link-nutricao.html" },
+        { codigogoogle: [""], nome: "Pedagogia",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "educação"], descricao: "Descrição do curso de Pedagogia.", link: "link-pedagogia.html" },
+        { codigogoogle: [""], nome: "Pedagogia EAD",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["ead", "educação"], descricao: "Descrição do curso de Pedagogia EAD.", link: "link-pedagogia-ead.html" },
+        { codigogoogle: [""], nome: "Psicologia",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "saúde"], descricao: "Descrição do curso de Psicologia.", link: "link-psicologia.html" },
+        { codigogoogle: [""], nome: "Sistemas de Informação",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "exatas"], descricao: "Descrição do curso de Sistemas de Informação.", link: "link-sistemas-informacao.html" },
+        { codigogoogle: [""], nome: "Recursos Humanos",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["graduação", "educação"], descricao: "Descrição do curso de Recursos Humanos.", link: "link-recursos-humanos.html" },
+        { codigogoogle: [""], nome: "Recursos Humanos EAD",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["ead", "educação"], descricao: "Descrição do curso de Recursos Humanos EAD.", link: "link-recursos-humanos-ead.html" },
+        { codigogoogle: [""], nome: "Pós Marketing",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["pos-graduação", "educação"], descricao: "Descrição do curso de Pós Marketing.", link: "link-pos-marketing.html" },
+        { codigogoogle: [""], nome: "Pós Equina",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["pos-graduação", "saúde"], descricao: "Descrição do curso de Pós Equina.", link: "link-pos-equina.html" },
+        { codigogoogle: [""], nome: "Pós Neuro",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["pos-graduação", "saúde"], descricao: "Descrição do curso de Pós Neuro.", link: "link-pos-neuro.html" },
+        { codigogoogle: [""], nome: "Pós PsicoEscolar",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["pos-graduação", "educação"], descricao: "Descrição do curso de Pós PsicoEscolar.", link: "link-pos-psicoescolar.html" },
+        { codigogoogle: [""], nome: "Pós MBAFinanças",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["pos-graduação", "exatas"], descricao: "Descrição do curso de Pós MBAFinanças.", link: "link-pos-mbafinancas.html" },
+        { codigogoogle: [""], nome: "Pós EstraFiscal",  presencialead: ["Presencial -"], tipodegraduacao: ["Bacharelado"], tags: ["pos-graduação", "exatas"], descricao: "Descrição do curso de Pós EstraFiscal.", link: "link-pos-estrafiscal.html" }
     ];
     
     function generateTagHTML(tags) {
@@ -56,11 +56,15 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
     cursos.forEach(curso => {
+
+
         const card = document.createElement('div');
         card.className = 'card';
+ 
         card.setAttribute('data-tags', curso.tags.join(',')); // Adiciona as tags ao card
         card.setAttribute('data-valor', curso.valor); // Adicione o valor do curso aqui
         card.setAttribute('data-semestres', curso.semestres); // Adiciona o número de semestres ao card
+        card.setAttribute('data-codigogoogle', curso.codigogoogle.join(',')); // Transforma o array em string
 
 
         card.innerHTML = `
@@ -130,5 +134,41 @@ document.addEventListener('DOMContentLoaded', function () {
             filterCards();
         });
     });
-});
+
+
+
+
+// mdsss ----------------------------------------------------------------------------------------------------------------------------------------- valores 
+
+     // Carrega os dados do CSV
+     const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vREDH7BTr9H2Um4BgNp45XYW1ybvS1qBIWACK2eElc7pmra17fwwcVBp3Um-GD6_wOF_N_FquXS1Rx9/pub?output=csv';
+
+     Papa.parse(url, {
+        download: true,
+        header: true,
+        complete: function(results) {
+            var data = results.data;
+    
+            data.forEach(row => {
+                document.querySelectorAll('.card').forEach(card => {
+                    const codigos = card.getAttribute('data-codigogoogle').split(',');
+                    if (codigos.includes(row['Cursos'])) {
+                        // Cria um elemento para o valor do curso
+                        let valorParagrafo = document.createElement('p');
+                        valorParagrafo.className = 'valor-curso';
+                        valorParagrafo.textContent = `${row['Cursos']}: ${row['Valor']}`;
+    
+                        // Adiciona o valor ao card
+                        card.appendChild(valorParagrafo);
+                    }
+                });
+            });
+        }
+    });
+    
+    
+ });
+
+
+
 
