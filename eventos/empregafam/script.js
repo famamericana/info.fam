386742998696d4fcae2b417fc790acbc251982ae
NICOLE HEGUY <nikkonh@hotmail.com>
2024-02-29 13:22:12 -0300

    att

--- eventos/empregafam/script.js
+++ eventos/empregafam/script.js
@@ -10,6 +10,11 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+    $("#BotaoTopo").load("/codigos-gerais/voltartopo/voltartopo.html");
+});
+
+
 //https forçar -------------------------------------------------------------------------------------------------------------------------------
 if (!location.href.startsWith("http://127.0") && location.protocol !== 'https:') {
     location.replace(`https:${location.href.substring(location.protocol.length)}`);
@@ -63,12 +68,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
         const description = e.currentTarget.querySelector('.logo-description');
         description.style.display = 'block';
 
+        
+        // Reseta os estilos para garantir que não haja estilos residuais de ajustes anteriores
+        description.style.position = 'absolute'; // Reseta para o valor padrão
+        description.style.left = '';
+        description.style.right = '';
+        description.style.top = '';
+        description.style.transform = '';
+
         // Calcula a posição do logo e ajusta a descrição se necessário
         const logoRect = e.currentTarget.getBoundingClientRect();
         const screenWidth = window.innerWidth;
         const descriptionWidth = 300; // Largura fixa da descrição
 
-        if (screenWidth < 550){
+        if (screenWidth < 550) {
             if (logoRect.right + descriptionWidth > screenWidth) {
                 // Centraliza a descrição na tela para telas menores que 400px
                 description.style.position = 'fixed'; // Usa posicionamento fixo para centralizar na tela
@@ -80,14 +93,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
         } else if (screenWidth < 1080) {
             if (logoRect.right + descriptionWidth > screenWidth) {
                 description.style.right = '0px';
-                description.style.left = `calc(-100% + 18px)`; 
-            } 
+                description.style.left = `calc(-100% + 18px)`;
+            }
         } else {
             // Posição padrão
             description.style.position = 'absolute'; // Retorna ao posicionamento absoluto ou original
             description.style.left = 'initial';
             description.style.transform = 'initial';
             description.style.right = 'initial';
+        }
+
+        // Adiciona evento para fechar a descrição
+        const closeButton = description.querySelector('.close-button');
+        if (closeButton) {
+            closeButton.onclick = (e) => {
+                description.style.display = 'none';
+                e.stopPropagation(); // Impede que o evento se propague para elementos pai
+            };
         }
 
         // Impede que o evento se propague
@@ -110,4 +132,34 @@ document.addEventListener('DOMContentLoaded', (event) => {
             desc.style.display = 'none';
         });
     }, { passive: true });
+});
+
+
+// ------------------------------------------------------------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Altura da navbar (ajuste conforme necessário)
+    var offsetHeight = 160; // Altura da sua navbar
+
+    // Função para realizar o scroll ajustado
+    function scrollToElement(elementId) {
+        const element = document.getElementById(elementId);
+        const elementPosition = element.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - offsetHeight;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
+    }
+
+    // Quando o botão Vagas é clicado
+    document.querySelector('.botaovagas').addEventListener('click', function() {
+        scrollToElement('vagas-section');
+    });
+
+    // Quando o botão Empresas é clicado
+    document.querySelector('.botaoempresas').addEventListener('click', function() {
+        scrollToElement('empresas-section');
+    });
 });
