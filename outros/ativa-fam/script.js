b3ab911f275d546d41c2d772c0022136f30ef559
NICOLE HEGUY <nikkonh@hotmail.com>
2024-03-11 20:07:42 -0300

    att e add emrpegafam e ativa fam

---
+++ outros/ativa-fam/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("#meuFooter").load("/codigos-gerais/footer/footer.html");
+});
+
+// Carregando o navbar e configurando o dropdown
+$(document).ready(function () {
+    $("#Navbar").load("/codigos-gerais/navbar-invertido/navbar.html", function () {
+        // Chama a função setupDropdown após o conteúdo ser carregado
+        setupDropdown();
+    });
+});
+
+$(document).ready(function () {
+    $("#BotaoTopo").load("/codigos-gerais/voltartopo/voltartopo.html");
+});
+
+
+//https forçar -------------------------------------------------------------------------------------------------------------------------------
+if (!location.href.startsWith("http://127.0") && location.protocol !== 'https:') {
+    location.replace(`https:${location.href.substring(location.protocol.length)}`);
+}
