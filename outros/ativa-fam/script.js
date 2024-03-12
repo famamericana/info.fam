db4989055b9a9929578ad69cba02549e6c4fa0fe
NICOLE HEGUY <nikkonh@hotmail.com>
2024-03-12 15:36:07 -0300

    att

--- outros/ativa-fam/script.js
+++ outros/ativa-fam/script.js
@@ -19,3 +19,43 @@ $(document).ready(function () {
 if (!location.href.startsWith("http://127.0") && location.protocol !== 'https:') {
     location.replace(`https:${location.href.substring(location.protocol.length)}`);
 }
+
+
+// logos looping --------------------------------------------------------------------------------------------------------------------------------------------------------
+document.addEventListener("DOMContentLoaded", function() {
+    const sliderWrapper = document.getElementById('logoSliderWrapper');
+    const logos = sliderWrapper.getElementsByClassName('logo');
+    const moveAmount = 1; // Velocidade de deslocamento dos logos em pixels
+    let positions = [];
+  
+    // Inicializa os logos e suas posições
+    function setupLogos() {
+      Array.from(logos).forEach((logo, index) => {
+        logo.style.backgroundImage = `url('${logo.dataset.logo}')`;
+        // Posiciona cada logo imediatamente após o anterior
+        positions[index] = index * (160 + 20); // Largura do logo + margem
+      });
+    }
+  
+    function slideLogos() {
+      positions = positions.map(position => position - moveAmount); // Move cada logo para a esquerda
+  
+      positions.forEach((position, index) => {
+        // Se o logo saiu completamente da tela pela esquerda
+        if (position < -150) {
+          const maxPosition = Math.max(...positions);
+          const wrapperWidth = sliderWrapper.offsetWidth;
+          // Calcula a nova posição do logo para que ele reapareça do lado direito
+          positions[index] = maxPosition + 150 + 20 > wrapperWidth ? maxPosition + 100 + 20 : wrapperWidth;
+        }
+        // Aplica a nova posição ao logo
+        logos[index].style.transform = `translateX(${positions[index]}px)`;
+      });
+  
+      requestAnimationFrame(slideLogos);
+    }
+  
+    setupLogos();
+    requestAnimationFrame(slideLogos);
+  });
+  
