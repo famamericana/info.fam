ef94431fae78f4a9c772e9eba6f43cc1ea5815f4
N1kkoin <nikkonh@hotmail.com>
2024-05-03 18:16:02 -0300

    Update script.js

--- paginicial/script.js
+++ paginicial/script.js
@@ -229,3 +229,5 @@ if (!location.href.startsWith("http://127.0") && location.protocol !== 'https:')
     location.replace(`https:${location.href.substring(location.protocol.length)}`);
 }
 
+
+//notificação ------------------------------------------------------------------------------------------------------------------------------------------------------------
