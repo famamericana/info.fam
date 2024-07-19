437a7ff09c29aef45d31e2053f6aa7a89216c1bc
N1kkoin <nikkonh@hotmail.com>
2024-07-19 14:23:29 -0300

    add até 1952

--- eventos/expo-atletas-olimpicos/script.js
+++ eventos/expo-atletas-olimpicos/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
     $("#meuFooter").load("/codigos-gerais/footer/footer.html");
 });
+
+
+// GALERIA ###############################################################################################################################################
+
+function changeImage(imageSrc) {
+    document.getElementById('mainImage').src = imageSrc;
+}
+
+// Set the initial main image to the first thumbnail on page load
+window.onload = function() {
+    changeImage('images/logos/1896-main.png');
+}
