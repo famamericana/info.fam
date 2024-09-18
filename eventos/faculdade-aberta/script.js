25454b94b031ef28b3eda1482b516aa8d8c69af1
N1kkoin <nikkonh@hotmail.com>
2024-09-18 15:15:58 -0300

    att

--- eventos/faculdade-aberta/script.js
+++ eventos/faculdade-aberta/script.js
@@ -17,3 +17,15 @@ if (!location.href.startsWith("http://127.0") && location.protocol !== 'https:')
 }
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    // Seleciona todos os cards, exceto o da div5
+    const cards = document.querySelectorAll('.myCard:not(.flipped)');
+    const flippedCard = document.querySelector('.div4 .myCard');
+
+    cards.forEach(card => {
+        card.addEventListener('mouseenter', function () {
+            // Remove a classe 'flipped' da div5 quando o mouse passar sobre outro card
+            flippedCard.classList.remove('flipped');
+        });
+    });
+});
