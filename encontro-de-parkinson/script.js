9ac6836e0304580536ba842fa4ae8411b590de06
N1kkoin <nikkonh@hotmail.com>
2025-02-19 17:18:24 -0300

    add encontro parkinson

---
+++ encontro-de-parkinson/script.js
@@ -0,0 +1,19 @@
+
+//https forçar -------------------------------------------------------------------------------------------------------------------------------
+if (!location.href.startsWith("http://127.0") && location.protocol !== 'https:') {
+    location.replace(`https:${location.href.substring(location.protocol.length)}`);
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+$(document).ready(function () {
+    $("#meuFooter").load("/codigos-gerais/footer-invertido/footer.html");
+});
+
+// Carregando o navbar e configurando o dropdown
+$(document).ready(function () {
+    $("#Navbar").load("/codigos-gerais/navbar-fam/navbar.html", function () {
+        // Chama a função setupDropdown após o conteúdo ser carregado
+        setupDropdown();
+    });
+});
