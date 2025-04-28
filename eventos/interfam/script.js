0f38324b344a0b16d86a3a3b6e17c9a74f3803dc
N1kkoin <nikkonh@hotmail.com>
2025-04-28 13:57:39 -0300

    att

---
+++ eventos/interfam/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $("#meuFooter").load("/codigos-gerais/footer-invertido/footer.html");
+});
+
+// Carregando o navbar e configurando o dropdown
+$(document).ready(function () {
+    $("#Navbar").load("/codigos-gerais/navbar/navbar.html", function () {
+        // Chama a função setupDropdown após o conteúdo ser carregado
+        setupDropdown();
+    });
+});
+
+
+//https forçar -------------------------------------------------------------------------------------------------------------------------------
+if (!location.href.startsWith("http://127.0") && location.protocol !== 'https:') {
+    location.replace(`https:${location.href.substring(location.protocol.length)}`);
+}
