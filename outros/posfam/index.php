b05651091f108906961a033845e2bfb3e69d2884
N1kkoin <nikkonh@hotmail.com>
2025-05-12 14:08:03 -0300

    att

--- outros/posfam/index.php
+++ outros/posfam/index.php
@@ -13,7 +13,7 @@ include_once "navfoot/navbar.php"
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <script src="script.js" defer></script>
-    <link rel="icon" href="images/famlogo.png">
+    <link rel="icon" href="/images/famlogo.png">
 </head>
 
 <body>
@@ -29,10 +29,12 @@ include_once "navfoot/navbar.php"
         <div class="bola bola-8"></div>
     </div>
 
-    <header>        <div class="titulo">
+    <header>
+        <div class="titulo">
             <h2><span style="color: #45b7e5">▪</span> Matrículas Abertas <span style="color: #45b7e5">▪</span></h2>
             <h1>Pós-Graduação é na FAM</h1>
-            <p>Aprimore sua carreira com professores atuantes no mercado de trabalho e em uma instituição de alta qualidade de ensino com responsabilidade social!</p>
+            <p>Aprimore sua carreira com professores atuantes no mercado de trabalho e em uma instituição de alta
+                qualidade de ensino com responsabilidade social!</p>
         </div>
         <div class="container-button">
             <a href="https://fam.inscricao.crmeducacional.com/login/48" class="button">Inscreva-se</a>
@@ -62,126 +64,184 @@ include_once "navfoot/navbar.php"
             <div class="container-cursos">
                 <h2 class="section-title">Nossos Cursos de Pós-Graduação</h2>
                 <p class="section-desc">Escolha entre diversos cursos de pós-graduação para impulsionar sua carreira</p>
-                
+
                 <div class="cards-container">
-                    <!-- Curso 1 -->
+                    <!-- Curso 8 -->
+                    <div class="card-curso">
+                        <span class="modalidade">Semipresencial</span>
+                        <h3>Biologia da Conservação</h3>
+                        <div class="info-curso">
+                            <span class="duracao"><i class="fa-regular fa-clock"></i> 3 aulas por mês - 24 meses</span>
+                        </div>
+                        <p class="descricao">Especialização em preservação e manejo sustentável da biodiversidade.</p>
+                        <div class="tags">
+                            <span class="tag">saúde</span>
+                            <span class="tag">ecologia</span>
+                            <span class="tag">sustentabilidade</span>
+                        </div>
+                        <div class="botoes-curso">
+                            <a href="https://www.fam.br/curso/biologia-da-conservacao/" class="btn-saiba-mais"
+                                target="_blank">Saiba mais</a>
+                            <a href="https://fam.inscricao.crmeducacional.com/login/48" class="btn-inscricao"
+                                target="_blank">Inscreva-se</a>
+                        </div>
+                    </div>
+
+                    <!-- Curso 1 
                     <div class="card-curso">
                         <span class="modalidade">Presencial</span>
                         <h3>Estratégia Fiscal e Tributária</h3>
                         <div class="info-curso">
                             <span class="duracao"><i class="fa-regular fa-clock"></i> Aulas semanais - 18 Meses</span>
-                        </div>                        <p class="descricao">Especialização em finanças e legislação fiscal.</p>                        <div class="tags">
+                        </div>
+                        <p class="descricao">Especialização em finanças e legislação fiscal.</p>
+                        <div class="tags">
                             <span class="tag">exatas</span>
                             <span class="tag">direito</span>
                             <span class="tag">contábil</span>
                         </div>
                         <div class="botoes-curso">
-                            <a href="https://www.fam.br/curso/estrategia-fiscal-e-tributaria/" class="btn-saiba-mais" target="_blank">Saiba mais</a>
-                            <a href="https://fam.inscricao.crmeducacional.com/login/48" class="btn-inscricao" target="_blank">Inscreva-se</a>
+                            <a href="https://www.fam.br/curso/estrategia-fiscal-e-tributaria/" class="btn-saiba-mais"
+                                target="_blank">Saiba mais</a>
+                            <a href="https://fam.inscricao.crmeducacional.com/login/48" class="btn-inscricao"
+                                target="_blank">Inscreva-se</a>
                         </div>
-                    </div>
-                    
-                    <!-- Curso 2 -->                    <div class="card-curso">
-                        <span class="modalidade">Presencial</span>
-                        <h3>MBA em Finanças, Investiment e Banking</h3>
+                    </div>-->
+
+                    <!-- Curso 2 -->
+                    <div class="card-curso">
+                        <span class="modalidade">Semipresencial</span>
+                        <h3>MBA em Finanças, Investimentos e Banking</h3>
                         <div class="info-curso">
                             <span class="duracao"><i class="fa-regular fa-clock"></i> Aulas quinzenais - 18 meses</span>
-                        </div>                        <p class="descricao">Especialização em finanças corporativas e investimentos.</p>                        <div class="tags">
+                        </div>
+                        <p class="descricao">Especialização em finanças corporativas e investimentos.</p>
+                        <div class="tags">
                             <span class="tag">exatas</span>
                             <span class="tag">mercado</span>
                             <span class="tag">gestão</span>
                         </div>
                         <div class="botoes-curso">
-                            <a href="https://www.fam.br/curso/mba-em-financas-investimentos-e-banking/" class="btn-saiba-mais" target="_blank">Saiba mais</a>
-                            <a href="https://fam.inscricao.crmeducacional.com/login/48" class="btn-inscricao" target="_blank">Inscreva-se</a>
+                            <a href="https://www.fam.br/curso/mba-em-financas-investimentos-e-banking/"
+                                class="btn-saiba-mais" target="_blank">Saiba mais</a>
+                            <a href="https://fam.inscricao.crmeducacional.com/login/48" class="btn-inscricao"
+                                target="_blank">Inscreva-se</a>
                         </div>
                     </div>
-                    
-                    <!-- Curso 3 -->                    <div class="card-curso">
+
+                    <!-- Curso 3 -->
+                    <div class="card-curso">
                         <span class="modalidade">Presencial</span>
                         <h3>Marketing e Negócios</h3>
                         <div class="info-curso">
-                            <span class="duracao"><i class="fa-regular fa-clock"></i> Aulas quinzenais ou semanais - 18 meses</span>
-                        </div>                        <p class="descricao">Foco em gestão de marketing e desenvolvimento de negócios.</p>                        <div class="tags">
+                            <span class="duracao"><i class="fa-regular fa-clock"></i> Duas vezes por semana - 18
+                                meses</span>
+                        </div>
+                        <p class="descricao">Foco em gestão de marketing e desenvolvimento de negócios.</p>
+                        <div class="tags">
                             <span class="tag">humanas</span>
                             <span class="tag">vendas</span>
                             <span class="tag">branding</span>
                         </div>
                         <div class="botoes-curso">
-                            <a href="https://www.fam.br/curso/marketing-e-negocios/" class="btn-saiba-mais" target="_blank">Saiba mais</a>
-                            <a href="https://fam.inscricao.crmeducacional.com/login/48" class="btn-inscricao" target="_blank">Inscreva-se</a>
+                            <a href="https://www.fam.br/curso/marketing-e-negocios/" class="btn-saiba-mais"
+                                target="_blank">Saiba mais</a>
+                            <a href="https://fam.inscricao.crmeducacional.com/login/48" class="btn-inscricao"
+                                target="_blank">Inscreva-se</a>
                         </div>
                     </div>
-                    
-                    <!-- Curso 4 -->                    <div class="card-curso">
+
+                    <!-- Curso 4 -->
+                    <div class="card-curso">
                         <span class="modalidade">Presencial</span>
                         <h3>Medicina Equina</h3>
                         <div class="info-curso">
-                            <span class="duracao"><i class="fa-regular fa-clock"></i> 20 horas de aula/mês - 20 meses</span>
-                        </div>                        <p class="descricao">Especialização no cuidado e tratamento de equinos.</p>                        <div class="tags">
+                            <span class="duracao"><i class="fa-regular fa-clock"></i> 3 aulas por mês - 24 meses</span>
+                        </div>
+                        <p class="descricao">Especialização no cuidado e tratamento de equinos.</p>
+                        <div class="tags">
                             <span class="tag">saúde</span>
                             <span class="tag">cirurgia</span>
                             <span class="tag">reprodução</span>
                         </div>
                         <div class="botoes-curso">
-                            <a href="https://www.fam.br/curso/medicina-equina/" class="btn-saiba-mais" target="_blank">Saiba mais</a>
-                            <a href="https://fam.inscricao.crmeducacional.com/login/48" class="btn-inscricao" target="_blank">Inscreva-se</a>
+                            <a href="https://www.fam.br/curso/medicina-equina/" class="btn-saiba-mais"
+                                target="_blank">Saiba mais</a>
+                            <a href="https://fam.inscricao.crmeducacional.com/login/48" class="btn-inscricao"
+                                target="_blank">Inscreva-se</a>
                         </div>
                     </div>
-                    
-                    <!-- Curso 5 -->                    <div class="card-curso">
+
+                    <!-- Curso 5 -->
+                    <div class="card-curso">
                         <span class="modalidade">Presencial</span>
                         <h3>Medicina de Animais Silvestres e Exóticos</h3>
                         <div class="info-curso">
-                            <span class="duracao"><i class="fa-regular fa-clock"></i> Especialização</span>
-                        </div>                        <p class="descricao">Cuidado e estudo de espécies nacionais e exóticas.</p>                        <div class="tags">
+                            <span class="duracao"><i class="fa-regular fa-clock"></i> 3 aulas por mês - 24 meses</span>
+                        </div>
+                        <p class="descricao">Cuidado e estudo de espécies nacionais e exóticas.</p>
+                        <div class="tags">
                             <span class="tag">saúde</span>
                             <span class="tag">biodiversidade</span>
                             <span class="tag">manejo</span>
                         </div>
                         <div class="botoes-curso">
-                            <a href="https://www.fam.br/curso/medicina-de-animais-silvestres-e-exoticos/" class="btn-saiba-mais" target="_blank">Saiba mais</a>
-                            <a href="https://fam.inscricao.crmeducacional.com/login/48" class="btn-inscricao" target="_blank">Inscreva-se</a>
+                            <a href="https://www.fam.br/curso/medicina-de-animais-silvestres-e-exoticos/"
+                                class="btn-saiba-mais" target="_blank">Saiba mais</a>
+                            <a href="https://fam.inscricao.crmeducacional.com/login/48" class="btn-inscricao"
+                                target="_blank">Inscreva-se</a>
                         </div>
                     </div>
-                    
-                    <!-- Curso 6 -->                    <div class="card-curso">
+
+                    <!-- Curso 6 -->
+                    <div class="card-curso">
                         <span class="modalidade">Presencial</span>
                         <h3>Neuropsicopedagogia Clínica e Psicomotricidade</h3>
                         <div class="info-curso">
                             <span class="duracao"><i class="fa-regular fa-clock"></i> Aulas quinzenais - 24 Meses</span>
-                        </div>                        <p class="descricao">Foco em aprendizagem e desenvolvimento cognitivo.</p>                        <div class="tags">
+                        </div>
+                        <p class="descricao">Foco em aprendizagem e desenvolvimento cognitivo.</p>
+                        <div class="tags">
                             <span class="tag">saúde</span>
                             <span class="tag">reabilitação</span>
                             <span class="tag">desenvolvimento</span>
                         </div>
                         <div class="botoes-curso">
-                            <a href="https://www.fam.br/curso/neuropsicopedagogia-clinica-e-psicomotricidade/" class="btn-saiba-mais" target="_blank">Saiba mais</a>
-                            <a href="https://fam.inscricao.crmeducacional.com/login/48" class="btn-inscricao" target="_blank">Inscreva-se</a>
+                            <a href="https://www.fam.br/curso/neuropsicopedagogia-clinica-e-psicomotricidade/"
+                                class="btn-saiba-mais" target="_blank">Saiba mais</a>
+                            <a href="https://fam.inscricao.crmeducacional.com/login/48" class="btn-inscricao"
+                                target="_blank">Inscreva-se</a>
                         </div>
                     </div>
-                    
-                    <!-- Curso 7 -->                    <div class="card-curso">
+
+                    <!-- Curso 7 -->
+                    <div class="card-curso">
                         <span class="modalidade">Presencial</span>
                         <h3>Psicologia Escolar e Educacional</h3>
                         <div class="info-curso">
-                            <span class="duracao"><i class="fa-regular fa-clock"></i> Aulas semanais - 18 meses</span>
-                        </div>                        <p class="descricao">Foco em desenvolvimento educacional e psicológico.</p>                        <div class="tags">
+                            <span class="duracao"><i class="fa-regular fa-clock"></i> Aulas quinzenais - 24 Meses</span>
+                        </div>
+                        <p class="descricao">Foco em desenvolvimento educacional e psicológico.</p>
+                        <div class="tags">
                             <span class="tag">humanas</span>
                             <span class="tag">aprendizagem</span>
                             <span class="tag">inclusão</span>
                         </div>
                         <div class="botoes-curso">
-                            <a href="https://www.fam.br/curso/psicologia-escolar-e-educacional/" class="btn-saiba-mais" target="_blank">Saiba mais</a>
-                            <a href="https://fam.inscricao.crmeducacional.com/login/48" class="btn-inscricao" target="_blank">Inscreva-se</a>
+                            <a href="https://www.fam.br/curso/psicologia-escolar-e-educacional/" class="btn-saiba-mais"
+                                target="_blank">Saiba mais</a>
+                            <a href="https://fam.inscricao.crmeducacional.com/login/48" class="btn-inscricao"
+                                target="_blank">Inscreva-se</a>
                         </div>
                     </div>
+
+
                 </div>
             </div>
         </section>
-      
+
     </main>
-<?php 
+    <?php 
 include_once "navfoot/footer.php"
 
 ?>
