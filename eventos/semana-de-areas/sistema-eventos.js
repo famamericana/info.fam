174850ac54a80de627cd4963c2482f50061bb345
N1kkoin <nikkonh@hotmail.com>
2025-10-22 14:20:53 -0300

    att

--- eventos/semana-de-areas/sistema-eventos.js
+++ eventos/semana-de-areas/sistema-eventos.js
@@ -49,7 +49,7 @@ class SemanaDeAreas {
         try {
             // Adiciona timestamp para forçar recarregamento do JSON
             const timestamp = Date.now();
-            const response = await fetch(`eventos-config.json?v=6.2`);
+            const response = await fetch(`eventos-config.json?v=6.3`);
             const data = await response.json();
             this.eventos = data.eventos;
             
